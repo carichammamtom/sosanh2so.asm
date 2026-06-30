22486a3c8f0d7bd605ac349bec3e4dd302354975
carichammamtom <vutrantrung1482008@gmail.com>
2026-06-30 10:00:49 +0700

    Add files via upload

---
+++ 1.Hello_word .asm
@@ -0,0 +1,23 @@
+ mov ah , 0eh
+ mov al,48h
+ int 10h
+  mov al,65h
+ int 10h
+  mov al,6ch
+ int 10h
+ int 10h
+  mov al,6fh
+ int 10h
+  mov al,20h
+ int 10h
+  mov al,77h
+ int 10h
+  mov al,6fh
+ int 10h
+  mov al,72h
+ int 10h
+  mov al,6ch
+ int 10h
+  mov al,64h
+ int 10h
+ 
